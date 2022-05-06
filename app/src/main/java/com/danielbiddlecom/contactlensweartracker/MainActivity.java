dc398da52db07aceac2757d7d5be65a71ad2d97f
biddlecom <36277663+biddlecom@users.noreply.github.com>
2022-05-06 18:37:50 -0400

    Calculate Average Wear Time
    -add more member variables.
    -add the Average Wear Time number TextView.
    -update explanation of how to calculate the Last five Wear Times.
    -add code to tell android what to do when the user presses the
    reset button.
    -When the user presses the reset button it will shift the numbers
    in the Last Five Wear Times to the right by one slot. It will also take
    note of the number noted in the slot and save it as both and integer
    and a string.
    -complete code that will calculate the Average Wear Time based on
    if the slot has a zero in it or a number greater that zero.  If a particular
    slot has a zero in it then it will most likely mean that the user has been
    using the app less than 10 weeks (on average).  Typically all Last Five
    Wear Times slots won't have a number in it until the user has been
    using the app for 10 weeks.  This is based on the average wear time
    of 2 weeks per pair of contact lens.
    -add String for error message toast

--- app/src/main/java/com/danielbiddlecom/contactlensweartracker/MainActivity.java
+++ app/src/main/java/com/danielbiddlecom/contactlensweartracker/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,15 +20,23 @@ public class MainActivity extends AppCompatActivity {
     TextView mMainLastFiveSlotThreeTV;
     TextView mMainLastFiveSlotFourTV;
     TextView mMainLastFiveSlotFiveTV;
+    TextView mAverageWearTimeNumberTV;
 
-    //Getting the variables that will hold the information in the textviews.
+    //Getting the member variables that will hold the information in the textviews.
     String mCurrentLensCountString;
+    String mLastFiveSlotOneString;
+    String mLastFiveSlotTwoString;
+    String mLastFiveSlotThreeString;
+    String mLastFiveSlotFourString;
+    String mLastFiveSlotFiveString;
+    String mAverageWearTimeString;
     int mCurrentLensCountInteger = 0;
     int mLastFiveSlotOneInteger = 0;
     int mLastFiveSlotTwoInteger = 0;
     int mLastFiveSlotThreeInteger = 0;
     int mLastFiveSlotFourInteger = 0;
     int mLastFiveSlotFiveInteger = 0;
+    int mAverageWearTimeInteger = 0;
 
     //Shared Prefs Member Variables.
     String mCurrentLensCountPref = "currentLensCountPref";
@@ -92,6 +101,9 @@ public class MainActivity extends AppCompatActivity {
         //Finding the Last Five Slot Five TextView.
         mMainLastFiveSlotFiveTV = findViewById(R.id.main_last_five_five);
 
+        //Finding the Average Wear Time number TextView.
+        mAverageWearTimeNumberTV = findViewById(R.id.average_wear_time_number_tv);
+
 
     } //End of OnCreate.
 
@@ -121,8 +133,8 @@ public class MainActivity extends AppCompatActivity {
             mMainLensCounterTV.setText(getString(R.string.main_lens_counter_placeholder));
         }
 
-        //TODO: Put this value in the shared prefs so it will be remembered when the user reopens
-        // the app in the future.
+        //TODO: Put this value (current days worn) in the shared prefs so it will be remembered when
+        // the user reopens the app in the future.
     }
 
     //This method will increase the number in the "Current Days Worn" counter TextView by "1".
@@ -142,51 +154,257 @@ public class MainActivity extends AppCompatActivity {
         //Setting the new value in the Current Lens Count TextView for the user to see.
         mMainLensCounterTV.setText(newString);
 
-        //TODO: Put this value in the shared prefs so it will be remembered when the user reopens
-        // the app in the future.
+        //TODO: Put this value (current days worn) in the shared prefs so it will be remembered when
+        // the user reopens the app in the future.
     }
 
     //This method has multiple steps.
     //
-    //FIRST- get the value from the MainLastFiveSlotFour TextView and convert it to an integer.
-    //Store that value in mLastFiveSlotFourInteger member value. Convert that integer back into
-    //a string and set it in the MainLastFiveSlotFive TextView.
+    //High Level picture: we are shifting the numbers of the Last Five
+    //Wear Times one spot to the right.  One will move to two, two will move to three, three will move
+    //to four, four will move to five and five will drop off and disappear. Spot one is currently
+    //vacant.  Spot one will be filled by the Current Days Worn value. To accomplish this we will
+    //have to work in reverse, starting with position five and working backwards to position one and
+    //then the Current Days Worn number.
     //
-    //SECOND- get the value from the MainLastFiveSlotThree TextView and convert it to an integer.
-    //Store that value in mLastFiveSlotThreeInteger member value. Convert that integer back into
-    //a string and set it in the MainLastFiveSlotFour TextView.
+    //Directions for working backwards...
+    //FIRST- get the value from the MainLastFiveSlotFour TextView and save it in the
+    //mLastFiveSlotFiveString. Next convert it to an integer and store that value in
+    //mLastFiveSlotFiveInteger member value. Take mLastFiveSlotFiveString and
+    //set it in the MainLastFiveSlotFive TextView.
     //
-    //THIRD- get the value from the MainLastFiveSlotTwo TextView and convert it to an integer.
-    //Store that value in mLastFiveSlotTwoInteger member value. Convert that integer back into
-    //a string and set it in the MainLastFiveSlotThree TextView.
+    //SECOND- get the value from the MainLastFiveSlotThree TextView and save it in the
+    //mLastFiveSlotFourString. Next convert it to an integer and store that value in
+    //mLastFiveSlotFourInteger member value. Take mLastFiveSlotFourString and
+    //set it in the MainLastFiveSlotFour TextView.
     //
-    // TODO: continue explaining what the method will do.
+    //THIRD- get the value from the MainLastFiveSlotTwo TextView and save it in the
+    //mLastFiveSlotThreeString. Next convert it to an integer and store that value in
+    //mLastFiveSlotThreeInteger member value. Take mLastFiveSlotThreeString and
+    //set it in the MainLastFiveSlotThree TextView.
     //
+    //FOURTH- get the value from the MainLastFiveSlotOne TextView and save it in the
+    //mLastFiveSlotTwoString. Next convert it to an integer and store that value in
+    //mLastFiveSlotTwoInteger member value. Take mLastFiveSlotTwoString and
+    //set it in the MainLastFiveSlotTwo TextView.
     //
+    //FIFTH- get the value from the Current Days Worn TextView and save it in the
+    //mLastFiveSlotOneString. Next convert it to an integer and store that value in
+    //mLastFiveSlotOneInteger member value. Take mLastFiveSlotOneString and
+    //set it in the MainLastFiveSlotOne TextView.
     //
+    //SIXTH- get all mLastFiveSlotIntegers (slots 1-5) and add them together.  Divide by 5.  This
+    //will give us the average number of days (Average Wear Time) of the users contact lenses.
+    //Convert the integer into a string and set it in the "average_wear_time_number_tv" textview.
     //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //Reset the number in the "Current Days Worn" counter TextView to "0".
+    //LASTLY- reset the number in the "Current Days Worn" counter TextView to "0" (zero).
     private void resetCurrentDaysWorn() {
 
-        //TODO: write the code following the explanation above.
+        //STEP ONE- getting the information from LastFiveSlotFour TextView and saving it in the
+        //mLastFiveSlotFiveString member variable.
+        mLastFiveSlotFiveString = mMainLastFiveSlotFourTV.getText().toString();
+        try {
+            //Parsing the string to get an integer and storing it in the LastFiveSlotFiveInteger variable.
+            mLastFiveSlotFiveInteger = Integer.parseInt(mLastFiveSlotFiveString);
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+        //Setting the new Slot Five value in the mMainLastFiveSlotFiveTV TextView for the user to see.
+        mMainLastFiveSlotFiveTV.setText(mLastFiveSlotFiveString);
 
+        //STEP TWO- getting the information from LastFiveSlotThree TextView and saving it in the
+        //mLastFiveSlotFourString member variable.
+        mLastFiveSlotFourString = mMainLastFiveSlotThreeTV.getText().toString();
+        try {
+            //Parsing the string to get an integer and storing it in the LastFiveSlotFourInteger variable.
+            mLastFiveSlotFourInteger = Integer.parseInt(mLastFiveSlotFourString);
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+        //Setting the new Slot Four value in the mMainLastFiveSlotFourTV TextView for the user to see.
+        mMainLastFiveSlotFourTV.setText(mLastFiveSlotFourString);
 
-        //Set the value of the "Current Days Worn" counter TextView to "0".
+        //STEP THREE- getting the information from LastFiveSlotTwo TextView and saving it in the
+        //mLastFiveSlotThreeString member variable.
+        mLastFiveSlotThreeString = mMainLastFiveSlotTwoTV.getText().toString();
+        try {
+            //Parsing the string to get an integer and storing it in the LastFiveSlotThreeInteger variable.
+            mLastFiveSlotThreeInteger = Integer.parseInt(mLastFiveSlotThreeString);
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+        //Setting the new Slot Three value in the mMainLastFiveSlotThreeTV TextView for the user to see.
+        mMainLastFiveSlotThreeTV.setText(mLastFiveSlotThreeString);
+
+        //STEP FOUR- getting the information from LastFiveSlotOne TextView and saving it in the
+        //mLastFiveSlotTwoString member variable.
+        mLastFiveSlotTwoString = mMainLastFiveSlotOneTV.getText().toString();
+        try {
+            //Parsing the string to get an integer and storing it in the LastFiveSlotTwoInteger variable.
+            mLastFiveSlotTwoInteger = Integer.parseInt(mLastFiveSlotTwoString);
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+        //Setting the new Slot Two value in the mMainLastFiveSlotTwoTV TextView for the user to see.
+        mMainLastFiveSlotTwoTV.setText(mLastFiveSlotTwoString);
+
+        //STEP FIVE- getting the information from Current Days Worn TextView and saving it in the
+        //mLastFiveSlotOneString member variable.
+        mLastFiveSlotOneString = mMainLensCounterTV.getText().toString();
+        try {
+            //Parsing the string to get an integer and storing it in the LastFiveSlotFiveInteger variable.
+            mLastFiveSlotOneInteger = Integer.parseInt(mLastFiveSlotOneString);
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+        //Setting the new Slot One value in the mMainLastFiveSlotOneTV TextView for the user to see.
+        mMainLastFiveSlotOneTV.setText(mLastFiveSlotOneString);
+
+        //STEP SIX- calling the method that will calculate the Average Wear Time based on the last five wear
+        //times and then show it is in the Average Wear Time number Textview.
+        calculateAverageWearTime();
+
+        //LASTLY- Set the value of the "Current Days Worn" counter TextView to "0" (zero).
         mMainLensCounterTV.setText(getString(R.string.main_lens_counter_placeholder));
 
-
+        //TODO- commit the Last Five Wear (slots 1-5) strings and the Average Wear Time to the Shared Prefs.
     }
 
-    //TODO: average out the wear times using the values in the LastFiveSlotIntegers and then put
-    // the new value in the average wear time number textview.
+    //This method will calculate the Average Wear Time of the user based on the Last Five Wear Times.
+    //Then it will convert that integer into a string and store it in the mAverageWearTimeString
+    //member variable so that it can later be set on the Average Wear Time number TextView.
+    private void calculateAverageWearTime() {
+        //Checking to see if any of the Last Five Wear Time numbers are zeros ("0").
+        //If they are zeros then the slot likely hasn't been filled in with user generated data yet.
+        //For example, if the user has only been using the app for a few weeks.
+        if(mLastFiveSlotOneInteger == 0 && mLastFiveSlotTwoInteger == 0 && mLastFiveSlotThreeInteger == 0
+        && mLastFiveSlotFourInteger ==0 && mLastFiveSlotFiveInteger == 0){
+            mAverageWearTimeNumberTV.setText(R.string.main_lens_counter_placeholder);
+        } else {
+            //Show a toast message letting the user know that something went wrong and to try again.
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.main_error_calculating_last_five), Toast.LENGTH_LONG).show();
+        }
+
+        //Checking to see if any of the Last Five Wear Time numbers are zeros ("0").
+        //If the first slot is not a zero and the last four are, then set the Average Wear Time to
+        //the value of slot one.
+        if(mLastFiveSlotOneInteger != 0 && mLastFiveSlotTwoInteger == 0 && mLastFiveSlotThreeInteger == 0
+                && mLastFiveSlotFourInteger ==0 && mLastFiveSlotFiveInteger == 0){
+            mAverageWearTimeNumberTV.setText(mLastFiveSlotOneString);
+        } else {
+            //Show a toast message letting the user know that something went wrong and to try again.
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.main_error_calculating_last_five), Toast.LENGTH_LONG).show();
+        }
+
+        //Checking to see if any of the Last Five Wear Time numbers are zeros ("0").
+        //If the first and second slots are not zeros and the last three are, then add the first two
+        //together and divide by 2 to get the Average Wear Time number.
+        if(mLastFiveSlotOneInteger != 0 && mLastFiveSlotTwoInteger != 0 && mLastFiveSlotThreeInteger == 0
+                && mLastFiveSlotFourInteger ==0 && mLastFiveSlotFiveInteger == 0){
+
+            //Adding the first two "Last Five Wear Times" slots up and storing it in the
+            //tempAverageWearTimeInteger.
+            int tempAverageWearTimeInteger = mLastFiveSlotOneInteger + mLastFiveSlotTwoInteger;
+
+            //Dividing the newly created tempAverageWearTimeInteger by 2 to get the average time of
+            //the Last Five Wear Times and then storing it in the mAverageWearTimeInteger.
+            mAverageWearTimeInteger = tempAverageWearTimeInteger / 2;
+
+            //Converting the mAverageWearTimeInteger to a String and storing it in the
+            //mAverageWearTimeString member variable.
+            mAverageWearTimeString = String.valueOf(mAverageWearTimeInteger);
+
+            //Setting the mAverageWearTimeString on the Average Wear Time number TextView.
+            mAverageWearTimeNumberTV.setText(mAverageWearTimeString);
+        } else {
+            //Show a toast message letting the user know that something went wrong and to try again.
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.main_error_calculating_last_five), Toast.LENGTH_LONG).show();
+        }
+
+        //Checking to see if any of the Last Five Wear Time numbers are zeros ("0").
+        //If the first, second and third slots are not zeros and the last two are, then add the
+        //first three together and divide by 3 to get the Average Wear Time number.
+        if(mLastFiveSlotOneInteger != 0 && mLastFiveSlotTwoInteger != 0 && mLastFiveSlotThreeInteger != 0
+                && mLastFiveSlotFourInteger ==0 && mLastFiveSlotFiveInteger == 0){
+
+            //Adding the first three "Last Five Wear Times" slots up and storing it in the
+            //tempAverageWearTimeInteger.
+            int tempAverageWearTimeInteger = mLastFiveSlotOneInteger + mLastFiveSlotTwoInteger
+                    + mLastFiveSlotThreeInteger;
+
+            //Dividing the newly created tempAverageWearTimeInteger by 3 to get the average time of
+            //the Last Five Wear Times and then storing it in the mAverageWearTimeInteger.
+            mAverageWearTimeInteger = tempAverageWearTimeInteger / 3;
+
+            //Converting the mAverageWearTimeInteger to a String and storing it in the
+            //mAverageWearTimeString member variable.
+            mAverageWearTimeString = String.valueOf(mAverageWearTimeInteger);
+
+            //Setting the mAverageWearTimeString on the Average Wear Time number TextView.
+            mAverageWearTimeNumberTV.setText(mAverageWearTimeString);
+        } else {
+            //Show a toast message letting the user know that something went wrong and to try again.
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.main_error_calculating_last_five), Toast.LENGTH_LONG).show();
+        }
+
+        //Checking to see if any of the Last Five Wear Time numbers are zeros ("0").
+        //If the first, second, third and fourth slots are not zeros and the last one is, then add
+        //the first four together and divide by 4 to get the Average Wear Time number.
+        if(mLastFiveSlotOneInteger != 0 && mLastFiveSlotTwoInteger != 0 && mLastFiveSlotThreeInteger != 0
+                && mLastFiveSlotFourInteger !=0 && mLastFiveSlotFiveInteger == 0){
+
+            //Adding the first four "Last Five Wear Times" slots up and storing it in the
+            //tempAverageWearTimeInteger.
+            int tempAverageWearTimeInteger = mLastFiveSlotOneInteger + mLastFiveSlotTwoInteger
+                    + mLastFiveSlotThreeInteger +mLastFiveSlotFourInteger;
+
+            //Dividing the newly created tempAverageWearTimeInteger by 4 to get the average time of
+            //the Last Five Wear Times and then storing it in the mAverageWearTimeInteger.
+            mAverageWearTimeInteger = tempAverageWearTimeInteger / 4;
+
+            //Converting the mAverageWearTimeInteger to a String and storing it in the
+            //mAverageWearTimeString member variable.
+            mAverageWearTimeString = String.valueOf(mAverageWearTimeInteger);
+
+            //Setting the mAverageWearTimeString on the Average Wear Time number TextView.
+            mAverageWearTimeNumberTV.setText(mAverageWearTimeString);
+        } else {
+            //Show a toast message letting the user know that something went wrong and to try again.
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.main_error_calculating_last_five), Toast.LENGTH_LONG).show();
+        }
+
+        //Checking to see if any of the Last Five Wear Time numbers are zeros ("0").
+        //If the first, second, third, fourth and fifth slots are all not zeros, then add
+        //them all together and divide by 5 to get the Average Wear Time number.
+        if(mLastFiveSlotOneInteger != 0 && mLastFiveSlotTwoInteger != 0 && mLastFiveSlotThreeInteger != 0
+                && mLastFiveSlotFourInteger !=0 && mLastFiveSlotFiveInteger != 0){
+
+            //Adding all the "Last Five Wear Times" slots up and storing it in the
+            //tempAverageWearTimeInteger.
+            int tempAverageWearTimeInteger = mLastFiveSlotOneInteger + mLastFiveSlotTwoInteger
+                    + mLastFiveSlotThreeInteger + mLastFiveSlotFourInteger + mLastFiveSlotFiveInteger;
+
+            //Dividing the newly created tempAverageWearTimeInteger by 5 to get the average time of
+            //the Last Five Wear Times and then storing it in the mAverageWearTimeInteger.
+            mAverageWearTimeInteger = tempAverageWearTimeInteger / 5;
+
+            //Converting the mAverageWearTimeInteger to a String and storing it in the
+            //mAverageWearTimeString member variable.
+            mAverageWearTimeString = String.valueOf(mAverageWearTimeInteger);
+
+            //Setting the mAverageWearTimeString on the Average Wear Time number TextView.
+            mAverageWearTimeNumberTV.setText(mAverageWearTimeString);
+        } else {
+            //Show a toast message letting the user know that something went wrong and to try again.
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.main_error_calculating_last_five), Toast.LENGTH_LONG).show();
+        }
+    }
 
 
 }
