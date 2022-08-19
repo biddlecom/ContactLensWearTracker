b8f721f8a4fe037080b2b66013e02f431f3c05a4
biddlecom <36277663+biddlecom@users.noreply.github.com>
2022-08-19 19:40:09 -0400

    Google Admob Main Activity
    -finish implementing Google Admob in Main Activity.
    -reconfigured activity_main.xml to handle proper
    ad placement.  This has a border at the top of the
    ad to give it room from the main app content. This
    is the preferred way to handle banner ads according
    to Google.
    -add dimens for Admob margin top.

--- app/src/main/java/com/danielbiddlecom/contactlensweartracker/MainActivity.java
+++ app/src/main/java/com/danielbiddlecom/contactlensweartracker/MainActivity.java
@@ -13,8 +13,10 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
@@ -149,20 +151,53 @@ public class MainActivity extends AppCompatActivity {
         //the app. (Checking to see if this is a new user).
         isTheUserNew();
 
-
-
-
-        //TODO  TESTING GOOGLE ADVIEW
+        //Google Admob ads.
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
 
+        //Finding the Adview and showing ads in it.
         mAdView = findViewById(R.id.google_ad_view);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        //Overrides for Google Admob.
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdImpression() {
+                // Code to be executed when an impression is recorded
+                // for an ad.
+            }
+
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+        });
 
     } //End of OnCreate.
 
